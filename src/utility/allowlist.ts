932df73841d583bf385b2b7fbe531d50e12a8e1b
netop://ウエハ <paul@webb.page>
2024-06-20 00:05:53 -0700

    updates allowlist

--- src/utility/allowlist.ts
+++ src/utility/allowlist.ts
@@ -4,9 +4,15 @@
 /// export
 
 export default [
+  /// batch 000
   "paul@webb.page",
   "h4ndsh4k@gmail.com",
   "hns@inspin.io",
   "me@jordanko.ch",
-  "2781@skiff.com"
+  "2781@skiff.com",
+  /// batch 001
+  "spencersolberg@outlook.com",
+  "peihsunk@gmail.com",
+  "lakeishashantelle@gmail.com",
+  "teleyinex@gmail.com"
 ];
